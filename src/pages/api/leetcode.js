6af56f94299a90643e55e29304d05cc80a31de62
Terry <shiyuntian@126.com>
2023-04-25 22:56:01 -0400

    add solution modal

--- src/pages/api/leetcode.js
+++ src/pages/api/leetcode.js
@@ -1,4 +1,8 @@
 import { Configuration, OpenAIApi } from 'openai';
+const { MongoClient } = require('mongodb');
+
+const connectionString =
+  'mongodb+srv://admin:H1iQFsiZdWNz095b@projects.xpyyyr0.mongodb.net/?retryWrites=true&w=majority';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -9,22 +13,38 @@ export default async function handler(req, res) {
   const { number, language } = req.query;
 
   try {
-    console.log(number, language);
-    res.status(200).json({ result: 'success' });
-    // const response = await openai.createChatCompletion({
-    //   model: 'gpt-3.5-turbo',
-    //   messages: [
-    //     {
-    //       role: 'user',
-    //       content: `Leetcode question ${number}, show me the ${language} solution code`,
-    //     },
-    //   ],
-    //   temperature: 0,
-    //   max_tokens: 2048,
-    //   top_p: 1,
-    //   frequency_penalty: 0,
-    // });
-    // res.status(200).json({ result: response.data.choices[0].message.content });
+    const client = new MongoClient(connectionString);
+    const db = client.db('leetcode');
+    const collection = db.collection('solutions');
+    const data = await collection.findOne({
+      number: parseInt(number),
+      language: language,
+    });
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: 'user',
+            content: `Leetcode question ${number}, show me the ${language} solution code`,
+          },
+        ],
+        temperature: 0,
+        max_tokens: 2048,
+        top_p: 1,
+        frequency_penalty: 0,
+      });
+      const newRecord = {
+        number: parseInt(number),
+        language,
+        solution: response.data.choices[0].message.content,
+      };
+      await collection.insertOne(newRecord);
+      res.status(200).json(newRecord);
+    }
+    await client.close();
   } catch (error) {
     console.error(`Error with request: ${error.message}`);
     res.status(500).json({
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import Image from 'next/image';
 import { useState } from 'react';
 import LeetCode from '../../public/assets/LeetCode.png';
+import Head from 'next/head';
+import { AiOutlineClose } from 'react-icons/ai';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 export default function Home() {
   const [number, setTitle] = useState('');
   const [language, setLanguage] = useState('javascript');
-
+  const [isVisible, setIsVisible] = useState(false);
+  const [data, setData] = useState('');
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
   };
@@ -15,81 +19,115 @@ export default function Home() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsVisible(true);
     const response = await fetch(
       `/api/leetcode?number=${number}&language=${language}`
     );
     const data = await response.json();
+    setData(data.solution);
     console.log(data);
   };
 
   return (
-    <div className='flex h-screen flex-col justify-center px-6 py-12 lg:px-8'>
-      <div className='mx-auto w-full max-w-sm mb-10'>
-        <Image src={LeetCode} width='640' height='64' alt='/' />
-      </div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div className='h-full flex flex-wrap items-center justify-center'>
-            <div className='my-3 sm:my-0'>
-              <label className='font-bold text-lg text-gray-700'>
-                Question number:
-              </label>
-              <input
-                type='number'
-                id='inputNumber'
-                name='inputNumber'
-                min='1'
-                max='2500'
-                required
-                className='rounded-md w-16 mx-2 p-1.5 bg-slate-100 text-gray-900 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                value={number}
-                onChange={handleTitleChange}
-              />
-            </div>
-            <div className='mx-5'>
-              <label className='font-bold text-lg text-gray-700'>
-                Language:
-              </label>
-              <select
-                className='rounded-md mx-2 p-2 text-gray-900 shadow-lg bg-slate-100'
-                value={language}
-                onChange={handleLanguageChange}
-              >
-                <option
-                  value='JavaScript'
-                  className='h-40 overflow-scroll scrollbar-thumb-gray-300 scrollbar-track-gray-100'
+    <div>
+      <div className='flex h-screen flex-col justify-center px-6 py-12 lg:px-8'>
+        <Head>
+          <title>Leetcode Solution</title>
+          <meta
+            name='description'
+            content='Leetcode solution with GPT-3.5-turbo'
+          />
+          <link rel='icon' href='/fav.webp' />
+        </Head>
+        <div className='mx-auto w-full max-w-sm mb-10'>
+          <Image src={LeetCode} width='640' height='64' alt='/' />
+        </div>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <div className='h-full flex flex-wrap items-center justify-center'>
+              <div className='my-3 sm:my-0'>
+                <label className='font-bold text-lg text-gray-700'>
+                  Question number:
+                </label>
+                <input
+                  type='number'
+                  id='inputNumber'
+                  name='inputNumber'
+                  min='1'
+                  max='2500'
+                  required
+                  className='rounded-md w-16 mx-2 p-1.5 bg-slate-100 text-gray-900 shadow-lg ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  value={number}
+                  onChange={handleTitleChange}
+                />
+              </div>
+              <div className='mx-5'>
+                <label className='font-bold text-lg text-gray-700'>
+                  Language:
+                </label>
+                <select
+                  className='rounded-md mx-2 p-2 text-gray-900 shadow-lg bg-slate-100'
+                  value={language}
+                  onChange={handleLanguageChange}
                 >
-                  JavaScript
-                </option>
-                <option value='Python'>Python</option>
-                <option value='Python3'>Python3</option>
-                <option value='java'>Java</option>
-                <option value='C++'>C++</option>
-                <option value='C'>C</option>
-                <option value='C#'>C#</option>
-                <option value='Ruby'>Ruby</option>
-                <option value='Swift'>Swift</option>
-                <option value='Go'>Go</option>
-                <option value='Scala'>Scala</option>
-                <option value='Kotlin'>Kotlin</option>
-                <option value='Rust'>Rust</option>
-                <option value='PHP'>PHP</option>
-                <option value='Typescript'>Typescript</option>
-                <option value='Racket'>Racket</option>
-                <option value='Erlang'>Erlang</option>
-                <option value='Elixir'>Elixir</option>
-                <option value='Dart'>Dart</option>
-              </select>
+                  <option value='JavaScript'>JavaScript</option>
+                  <option value='Python'>Python</option>
+                  <option value='Python3'>Python3</option>
+                  <option value='java'>Java</option>
+                  <option value='C++'>C++</option>
+                  <option value='C'>C</option>
+                  <option value='C#'>C#</option>
+                  <option value='Ruby'>Ruby</option>
+                  <option value='Swift'>Swift</option>
+                  <option value='Go'>Go</option>
+                  <option value='Scala'>Scala</option>
+                  <option value='Kotlin'>Kotlin</option>
+                  <option value='Rust'>Rust</option>
+                  <option value='PHP'>PHP</option>
+                  <option value='Typescript'>Typescript</option>
+                  <option value='Racket'>Racket</option>
+                  <option value='Erlang'>Erlang</option>
+                  <option value='Elixir'>Elixir</option>
+                  <option value='Dart'>Dart</option>
+                </select>
+              </div>
+              <div>
+                <input
+                  className='shadow-md p-2 shadow-gray-400 rounded-lg text-gray-700 font-bold cursor-pointer'
+                  type='submit'
+                  value='Search'
+                />
+              </div>
             </div>
-            <div>
-              <input
-                className='shadow-md p-2 shadow-gray-400 rounded-lg text-gray-700 font-bold cursor-pointer'
-                type='submit'
-                value='Search'
-              />
+          </form>
+        </div>
+      </div>
+      <div
+        className={
+          isVisible
+            ? 'fixed left-0 top-0 w-full h-screen bg-black/50'
+            : 'hidden'
+        }
+      >
+        <div className='flex justify-center items-center w-full h-full'>
+          <div className='w-3/5 h-4/5 bg-slate-100 rounded-xl shadow-lg'>
+            <div class='flex justify-end'>
+              <button
+                onClick={() => {
+                  setIsVisible(false);
+                }}
+                class='rounded-full shadow-gray-400 bg-slate-100 w-8 h-8'
+              >
+                <AiOutlineClose />
+              </button>
+            </div>
+            <div className='flex justify-center w-full h-full'>
+              <div className='p-3 mt-2 bg-slate-200 shadow-lg rounded-lg overflow-auto'>
+                <ReactMarkdown>{data}</ReactMarkdown>
+              </div>
             </div>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
